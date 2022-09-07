2dc30a5ecf0e99929bd0017d999f1d080fc3e8d2
student <student@C251U45>
2022-09-07 11:31:05 +0300

    Добавьте файлы проекта.

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,16 @@
+﻿// helloworld.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 30; i++)
+	{
+		Beep(400 + i * 50, 1000 - i * 40);
+   }
+}
+
